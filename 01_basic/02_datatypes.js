d609b5b11208516288a2f6ff7859ef8100537bf5
priteshvyas <priteshvyas0809@gmail.com>
2025-05-05 20:17:32 +0530

    datatypes in javascript

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,41 @@
+//datatypes in javascript
+
+//primitive datatype
+console.log("")
+let name ="pritesh" // String datatype "" or ''
+let age=23 //number datatype => 2 to power 53 and then bigint is used
+let isLoginUser=true // boolean datatype which has value true or false
+let temperature =null // null is standalone value 
+let state; // undefind datatype
+//symbol => used for unique 
+
+//non-primitive datatype
+let obb={name: "Pritesh",
+    age: 24,
+    isStudent: true} //object with key and value
+
+  
+
+
+// how to check typeof variable?
+console.log("typeof variable...")
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoginUser)
+console.log(typeof temperature) 
+console.log(typeof state) // 
+console.log(typeof obb);
+
+console.log("")
+console.log("typeof undefind and null")
+
+
+console.log(typeof undefined); //type of undefind will be undefind
+console.log(typeof null);// null but it will give typeof is object
+
+
+
+
+
+
+
