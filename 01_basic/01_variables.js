5e2b697b8e3ce2632da402d758634d4531830f65
priteshvyas <priteshvyas0809@gmail.com>
2025-05-05 19:27:35 +0530

    varaiables in javascript

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,28 @@
+const accountId=3636467
+let accountName="Pritesh"
+var accountAge=23
+accountCity="Rajkot"
+
+// now changing the value of each variables
+
+// accountId=22353 this will not work due constant value cant be change
+console.log(accountId);
+
+//let accountName="Om" we cant redeclare variable using let
+accountName="Om"
+console.log(accountName);
+
+//Problem using var is when  when we use same name variable in another file or block it will change the value of first one
+accountAge=24
+console.log(accountAge);
+
+accountCity="Vadodra"
+console.log(accountCity);
+
+
+// using console.table multiple variable are printed at same time and give table form
+console.table([accountId,accountName,accountAge,accountCity])
+
+
+
+
