1850fd7788b8a7aaadcbd59d9ee23d3a8bce510f
priteshvyas <priteshvyas0809@gmail.com>
2025-05-05 20:51:50 +0530

    converstion in javascript

---
+++ 01_basic/03_converstion.js
@@ -0,0 +1,45 @@
+// converstion operation
+
+// possible outcomes of string to number converstion
+let name ="pritesh"
+
+let isNameNumber =Number(name)
+console.log(typeof isNameNumber);
+console.log(isNameNumber);
+
+//"33"=33
+//"33abc"=NaN
+//true=1
+//false=0
+
+console.log("");
+
+// converstion from string to boolean
+let isLoginUser ="pritesh"
+// let isLoginUser ="" 
+
+let booleanIsLoginUser = Boolean(isLoginUser)
+console.log(typeof booleanIsLoginUser);
+console.log(booleanIsLoginUser);
+
+// "string"=true
+//""=false
+//1=true
+//0=false
+console.log("");
+
+//converstion number to string
+
+let number = 33
+let numberString= String(number)
+console.log(typeof numberString);
+console.log(numberString);
+
+
+
+
+
+
+
+
+
