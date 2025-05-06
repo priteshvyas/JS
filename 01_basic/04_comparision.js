6ba8091d7c484217cb712716a881dcd78c46157a
priteshvyas <priteshvyas0809@gmail.com>
2025-05-06 19:45:55 +0530

    comparision with null undefind and = and ===

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,46 @@
+console.log(1>2);
+console.log(1<2);
+console.log(1==2);
+console.log(1!=2);
+
+console.log("");
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+
+console.log("");
+
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//so they behave diffrently in comparison and arithmetic operation
+
+
+console.log("");
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("");
+
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
+
+
