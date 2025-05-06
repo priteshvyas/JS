35cdd8f32f1d227e91e299a0530a9c96b9b5e4c1
priteshvyas <priteshvyas0809@gmail.com>
2025-05-06 19:08:41 +0530

    operation added with converstion

--- 01_basic/03_converstion.js
+++ 01_basic/03_converstion.js
@@ -37,6 +37,63 @@ console.log(numberString);
 
 
 
+//******************************************* operation***********************************************
+console.log("");
+
+let value =3
+let negValue= -value
+console.log(negValue); // it will give output -3
+
+console.log("");
+
+let str1 ="pritesh"
+let str2 =" vyas"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("");
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2");  //32
+
+console.log("");
+
+console.log(true); //true
+console.log(+true); //1
+console.log(+""); //0
+
+
+console.log("");
+
+let x=3
+let y=x++
+console.log(x , y);
+
+let p=3
+let q=++p
+console.log(p , q);
+
+let s = 3
+let m = s--
+console.log(s , m);
+
+let ss = 3
+let mm = --ss
+console.log(ss , mm);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
